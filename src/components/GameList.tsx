93b825b4ba4e25a521d7619f4c67bf23de70aa26
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2023-09-07 11:31:40 +0200

    removed the # in onError src

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -28,7 +28,7 @@ export default function GameList({ games }: GameListProps) {
                   onError={(e) => {
                     const target = e.target as HTMLInputElement;
                     target.onerror = null;
-                    target.src = "#"; // link to fallback omg
+                    target.src = ""; // link to fallback img
                   }}
                 />
                 <div className="card-body">
