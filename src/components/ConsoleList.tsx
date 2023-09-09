14dbd7397efe66868cda64f1e543ad3540e6d86c
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2023-09-09 21:33:00 +0200

    some styling

--- src/components/ConsoleList.tsx
+++ src/components/ConsoleList.tsx
@@ -55,46 +55,52 @@ export default function Consoles() {
   };
 
   return (
-    <div className="container mt-4">
-      <div className="row">
-        {/* Left column for ConsoleList */}
-        <div className="col-md-2">
-          <div className="d-flex flex-column">
-            {consoles
-              .slice(0, showAllConsoles ? consoles.length : 4)
-              .map((console, i) => (
-                <div
-                  key={console.id}
-                  className={`mb-2 ${
-                    selectedConsoleIndex === i ? "bg-light" : ""
-                  }`}
-                  onClick={() => handleConsoleClick(i)}
+    <>
+      <div className="container mt-4">
+        <div className="row">
+          {/* Left column for ConsoleList */}
+          <div className="col-md-2">
+            <h4>Consoles</h4>
+            <div className="d-flex flex-column">
+              {consoles
+                .slice(0, showAllConsoles ? consoles.length : 4)
+                .map((console, i) => (
+                  <div
+                    key={console.id}
+                    className={`mb-2 ${
+                      selectedConsoleIndex === i ? "bg-light" : ""
+                    }`}
+                    onClick={() => handleConsoleClick(i)}
+                  >
+                    <div className="p-2">{console.name}</div>
+                  </div>
+                ))}
+              {!showAllConsoles && (
+                <button
+                  className="btn btn-primary"
+                  onClick={handleShowAllConsoles}
                 >
-                  <div className="p-2">{console.name}</div>
-                </div>
-              ))}
-            {!showAllConsoles && (
-              <button className="btn btn-link" onClick={handleShowAllConsoles}>
-                Show More
-              </button>
+                  Show More
+                </button>
+              )}
+            </div>
+          </div>
+          {/* Right column for GameList */}
+          <div className="col-md-10">
+            {selectedConsoleIndex !== null ? (
+              <div className="games-for-console">
+                <h3>{`Games for ${consoles[selectedConsoleIndex].name}`}</h3>
+                <GameList
+                  games={consoles[selectedConsoleIndex].games}
+                  selectedConsole={consoles[selectedConsoleIndex]}
+                />
+              </div>
+            ) : (
+              <GameList games={[]} selectedConsole={null} />
             )}
           </div>
         </div>
-        {/* Right column for GameList */}
-        <div className="col-md-10">
-          {selectedConsoleIndex !== null ? (
-            <div className="games-for-console">
-              <h3>{`Games for ${consoles[selectedConsoleIndex].name}`}</h3>
-              <GameList
-                games={consoles[selectedConsoleIndex].games}
-                selectedConsole={consoles[selectedConsoleIndex]}
-              />
-            </div>
-          ) : (
-            <GameList games={[]} selectedConsole={null} />
-          )}
-        </div>
       </div>
-    </div>
+    </>
   );
 }
